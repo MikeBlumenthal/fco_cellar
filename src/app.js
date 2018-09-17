084415be9fd95855cf73995fb9b9cda82cdfe246
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-17 08:34:09 +0100

    adds list view and methods

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const Cellar = require('./models/cellar.js');
 const SelectView = require('./views/select_view.js');
+const ListView = require('./views/list_view.js');
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const select = document.querySelector('#choices');
   const selectView = new SelectView(select);
   selectView.bindEvents();
+
+  const listView = new ListView();
+  listView.bindEvents();
+
 
   const cellar = new Cellar();
   cellar.getData();
--- src/views/list_view.js
+++ src/views/list_view.js
@@ -1,1 +1,17 @@
 const PubSub = require('../helpers/pub_sub.js');
+
+const ListView = function () {
+
+}
+
+ListView.prototype.bindEvents = function () {
+  PubSub.subscribe('Cellar:selected-ready', (event) => {
+    this.display(event.detail);
+  })
+}
+
+ListView.prototype.display = function (data) {
+  console.log(data);
+};
+
+module.exports = ListView;
